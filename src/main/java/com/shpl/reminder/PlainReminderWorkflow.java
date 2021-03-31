94437a5dc42c28b19f1f7139390828c263ebf3bd
Sergio López <sergio.lopezdelaflor@masmovil.com>
2021-03-31 10:07:22 +0200

    Add Signal

--- src/main/java/com/shpl/reminder/PlainReminderWorkflow.java
+++ src/main/java/com/shpl/reminder/PlainReminderWorkflow.java
@@ -11,19 +11,28 @@ public class PlainReminderWorkflow implements ReminderWorkflow {
 
     private final LogActivities activities;
     private int nReminds;
+    private boolean execute;
 
     public PlainReminderWorkflow() {
         this.activities = Workflow.newActivityStub(LogActivities.class);
         this.nReminds = 0;
+        this.execute = true;
     }
 
     @Override
     public void remind() {
         LOGGER.info(activities.init());
-        while (nReminds < 20) {
+        LOGGER.info("Workflow id {{}}", Workflow.getWorkflowInfo().getWorkflowId());
+        while (nReminds < 20 && execute) {
             Workflow.sleep(Duration.ofSeconds(5));
             nReminds++;
             LOGGER.info(activities.run(nReminds));
         }
+    }
+
+    @Override
+    public void kill() {
+        LOGGER.info("Stopping...");
+        execute = false;
     }
 }
--- src/main/java/com/shpl/reminder/ReminderWorkflow.java
+++ src/main/java/com/shpl/reminder/ReminderWorkflow.java
@@ -1,8 +1,12 @@
 package com.shpl.reminder;
 
+import com.uber.cadence.workflow.SignalMethod;
 import com.uber.cadence.workflow.WorkflowMethod;
 
 public interface ReminderWorkflow {
     @WorkflowMethod(executionStartToCloseTimeoutSeconds = 300, taskList = Constant.TASK_LIST)
     void remind();
+
+    @SignalMethod
+    void kill();
 }
